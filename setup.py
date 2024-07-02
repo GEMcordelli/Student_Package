bd0a699a18a7e5b7d83e05efa11f25c2d063e1b5
Gabriella <gec2tp@virginia.edu>
2024-07-02 11:15:27 -0400

    adding my package to student_package

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+name = 'Test',
+version = '0.1',
+desrciption = 'demonstration package of a conversation starter',
+url = 'https://github.com/GEMcordelli/Student_Package',
+authort='Gabby Cordelli',
+author_email = 'grozzy78@gmail.com',
+license = 'MIT',
+packages=['mypackage'])
